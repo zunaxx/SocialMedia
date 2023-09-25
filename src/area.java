cd139a307415d2fe94f27de79e2a1d4ca734410c
zunaxx <127804550+zunaxx@users.noreply.github.com>
2023-09-26 00:25:51 +0600

    Update area.java

--- src/area.java
+++ src/area.java
@@ -1,11 +1,1 @@
-public class area {
-    public static void main (String[] args){
-        Trangle trangle = new Trangle();
-        trangle.a = 10;
-        trangle.b = 7;
-        trangle.c = 5;
 
-        System.out.println(trangle.a*trangle.b/2);
-    }
-
-}
